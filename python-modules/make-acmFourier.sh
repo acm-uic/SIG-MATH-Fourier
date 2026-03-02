b9b1c1d324aeab8b1da46f143179354fb208d334
Lam Nguyen <lamtnguyen989@gmail.com>
2026-03-02 15:47:13 -0600

    cleanups

--- python-modules/make-acmFourier.sh
+++ python-modules/make-acmFourier.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # This is meant to be run from directory root
-# i.e. run it like `./python-modules/make-acmFourier.sh` from SIG-MATH-Fourier/ directory
+# i.e. run it like `./python-PY_MODULEs/make-acmFourier.sh` from SIG-MATH-Fourier/ directory
 
 # Names and locations
-MODULE_NAME="acmFourier"
-MODULE_DIR="python-modules"
+PY_MODULE_NAME="acmFourier"
+PY_MODULE_DIR="python-PY_MODULEs"
 
 # Setting Python environments
 python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
-pip install -r $MODULE_DIR/requirements.txt
+pip install -r $PY_MODULE_DIR/requirements.txt
 
 # Binding the C++ to Python
 g++ -O3 -Wall -shared -std=c++17 -fPIC \
     $(python -m pybind11 --includes) \
     -I src/ \
-    $MODULE_DIR/$MODULE_NAME.cpp \
-    -o $MODULE_DIR/$MODULE_NAME$(python3-config --extension-suffix)
+    $PY_MODULE_DIR/$PY_MODULE_NAME.cpp \
+    -o $PY_MODULE_DIR/$PY_MODULE_NAME$(python3-config --extension-suffix)
--- python-modules/make-acmFourierCUDA.sh
+++ python-modules/make-acmFourierCUDA.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # This is meant to be run from directory root
-# i.e. run it like `./python-modules/make-acmFourierCUDA.sh` from SIG-MATH-Fourier/ directory
+# i.e. run it like `./python-PY_MODULEs/make-acmFourierCUDA.sh` from SIG-MATH-Fourier/ directory
 
 # Names and locations
-MODULE_NAME="acmFourierCUDA"
-MODULE_DIR="python-modules"
+PY_MODULE_NAME="acmFourierCUDA"
+PY_MODULE_DIR="python-PY_MODULEs"
 
 # Setting Python environments
 python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
-pip install -r $MODULE_DIR/requirements.txt
+pip install -r $PY_MODULE_DIR/requirements.txt
 
 # Binding the C++ to Python
 nvcc -O3 -shared -std=c++20 \
     src/fourier.cu \
     -I src/ \
-    $MODULE_DIR/$MODULE_NAME.cpp \
+    $PY_MODULE_DIR/$PY_MODULE_NAME.cpp \
     -Xcompiler "-fPIC -Wall -Werror -Wextra -O3" \
     $(python -m pybind11 --includes) \
-    -o $MODULE_DIR/$MODULE_NAME$(python3-config --extension-suffix) \
+    -o $PY_MODULE_DIR/$PY_MODULE_NAME$(python3-config --extension-suffix) \
     -lcudart
